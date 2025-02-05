bfef69192f32ccc5f23978a85a4145124b0694d5
CPenalba <carolinapenalbacorpa@gmail.com>
2025-02-05 13:18:16 +0100

    Add project files.

---
+++ MvcCoreLinqToSQL/Controllers/EnfermosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreLinqToSQL.Models;
+using MvcCoreLinqToSQL.Repositories;
+
+namespace MvcCoreLinqToSQL.Controllers
+{
+    public class EnfermosController : Controller
+    {
+        RepositoryEnfermos repo;
+        public EnfermosController()
+        {
+            this.repo = new RepositoryEnfermos();
+        }
+
+        public IActionResult Index()
+        {
+            List<Enfermo> enfermos = this.repo.GetEnfermos();
+            return View(enfermos);
+        }
+
+        public IActionResult Details(string inscripcion)
+        {
+            Enfermo enfermo = this.repo.FindEnfermo(inscripcion);
+            return View(enfermo);
+        }
+
+        public IActionResult Delete(int inscripcion)
+        {
+            this.repo.DeleteEnfermo(inscripcion);
+            return RedirectToAction("Index");
+        }
+    }
+}
